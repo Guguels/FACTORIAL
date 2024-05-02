68749d4bbe1ea166455ae88b4f7a933f76c98cec
Guguels <151414937+Guguels@users.noreply.github.com>
2024-05-01 20:12:50 -0400

    Add files via upload

---
+++ factorial.js
@@ -0,0 +1,6 @@
+document.write("<h1>Factorial</h1>");
+var N = prompt("Introduce N:");
+var P = Math.sqrt(2 * Math.PI * N);
+var T = Math.pow(N / Math.E, N);
+var S = P * T;
+document.write("<h3>El factorial es: " + S + "</h3>");
